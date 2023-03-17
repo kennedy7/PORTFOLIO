224efa0a2bdbdcf86dd4affc230a0e8592afcf83
unknown <kennedyhillary6@gmail.com>
2023-03-17 16:07:04 +0100

    refactor

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-const express = require ('express')
+const express = require("express");
 const session = require("express-session");
 const flash = require("express-flash");
-const { router } = require('./route')
-const PORT = process.env.PORT || 4000
-const app = express()
+const { router } = require("./route");
+const PORT = process.env.PORT || 4000;
+const path = require("path");
+const app = express();
 
-//view engine
-app.set("view engine", "ejs");
-app.use(express.static('public'));
+// //view engine
+// app.set("view engine", "html");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
-app.listen (PORT, ()=>{console.log(`server started on port ${PORT}`)})
-
-app.use(
-    session({
-      secret: "secret",
-      resave: false,
-      saveUninitialized: false
-    })
-  )
-
-app.use(flash())
-
-app.get("/", function (req, res) {
-    res.render('cv')
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
+app.use(flash());
+
+app.get("/", function (req, res) {
+  // res.render("/index.html");
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static("public"));
+// app.use(express.static(__dirname + "/views"));
 
 app.use(express.json());
 
-app.use(router)
+app.use(router);
--- route.js
+++ route.js
@@ -1,39 +1,39 @@
-const express = require ('express');
-const router = express.Router()
-const nodemailer = require ('nodemailer')
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+const path = require("path");
 
-router.post("/contact", (req, res)=>{
-    const { name, email, subject, message} = req.body
-    console.log(req.body)
-//MAIL SETUP
-var mail = nodemailer.createTransport({
-    service: 'gmail',
+router.post("/contact", (req, res) => {
+  const { name, email, subject, message } = req.body;
+  console.log(req.body);
+  //MAIL SETUP
+  var mail = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-        user: 'mytechportfolio1@gmail.com',
-        pass: 'otpcgoekchmnwoyt'
+      user: "mytechportfolio1@gmail.com",
+      pass: "otpcgoekchmnwoyt",
     },
+  });
+
+  var mailOptions = {
+    from: email,
+    to: "mytechportfolio1@gmail.com",
+    subject: `[PORTFOLIO MAIL] SUBJECT: ${subject}`,
+    text: `This is ${name}, ${message}. Reach back through ${email} ,`,
+  };
+
+  mail.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log("mailing error");
+      res.send("mailing error, Please try again");
+    } else {
+      console.log("Email sent successfully:" + data.response);
+      // req.flash("Mail has been sent to KENNEDY HILLARY");
+      // res.redirect("/");
+      res.sendFile(path.join(__dirname + "/views/emailsent.html"));
+      //   res.sendFile("/emailsent.html");
+    }
+  });
 });
 
-var mailOptions = {
-    from: email,
-    to:  'mytechportfolio1@gmail.com',
-    subject: `[PORTFOLIO MAIL] SUBJECT: ${subject}`,
-    text: `This is ${name}, ${message}. Reach back through ${email} ,`
-   
-    
-};
-
-mail.sendMail(mailOptions, (err, data)=> {
-    if (err) {
-        console.log('mailing error');
-        res.send('invalid mail')
-    } else {
-        console.log('Email sent successfully:' + data.response);
-        // req.flash("Mail has been sent to KENNEDY HILLARY");
-        // res.redirect("/");
-        res.render('emailsent')
-    }
-})
-})
-
-module.exports = {router}
+module.exports = { router };
